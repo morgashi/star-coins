179cc318abe1e39dfa0d00fc3289a172ab89418c
morgashi <fordmorgan027@gmail.com>
2026-03-21 23:15:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const loginBtn = document.getElementById('loginBtn')
 const logoutBtn = document.getElementById('logoutBtn')
+const usernameInput = document.getElementById("username")
+const welcomeText = document.getElementById("welcomeText")
 
 const loginScreen = document.getElementById('loginScreen')
 const dashboard = document.getElementById('dashboard')
 
+window.onload = function() {
+    const savedUser = this.localStorage.getItem("user")
+    if(savedUser) {
+        showDasboard(savedUser)
+    }
+}
+
+
 loginBtn.onclick = function() {
+    const username = usernameInput.value
+
+    if (username === "") {
+        alert("Enter a username")
+        return
+    }
+
+    localStorage.setItem("user", username)
+    showDasboard(username)
+}
+
+function showDasboard(user) {
     loginScreen.style.display = "none"
-    dashboard.style.display = 'block'
+    dashboard.style.display = "block"
+    welcomeText.innerText = "Welcome, " + user
 }
 
-logoutBtn.onclick = function() {
+function showLogin() {
     dashboard.style.display = "none"
-    loginScreen.style.display = 'block'
+    loginScreen.style.display = "block"
 }
-
-localStorage.setItem("user", username)
