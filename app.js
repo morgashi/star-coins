8f531476ca120acb926f163532b5e090e9b2bd80
morgashi <fordmorgan027@gmail.com>
2026-03-22 18:15:05 -0400

    ver3 app.js

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-const loginBtn = document.getElementById('loginBtn')
-const logoutBtn = document.getElementById('logoutBtn')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Star Coins</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-const loginScreen = document.getElementById('loginScreen')
-const dashboard = document.getElementById('dashboard')
+<body>
+    <div id="app">
+        <!-- LOGIN SCREEN -->
+        <div id="loginScreen">
+            <h1>Star Coins</h1>
+            <input id="username" placeholder="Username">
+            <input id="password" type="password" placeholder="Password">
+            <button id="loginBtn">Login</button>
+        </div>
+        <!-- HOMEPAGE-->
+         <div id="dashboard" style="display:none;">
+            <nav id="navbar">
+                <span class="nav-brand">★ Star Coins</span>
+                <div class="nav-links">
+                    <span>Dashboard</span>
+                </div>
+                <div class="nav-right">
+                    <span id="welcomMsg"></span>
+                    <button id="logoutBtn">Logout</button>
+                </div>
+            </nav>
+            
+            <div class="main-grid">
 
-loginBtn.onclick = function() {
-    const username = document.getElementById('username').value;
-    localStorage.setItem("user", username);   // moved here, runs on login
+                <div class="panel" id="accountsPanel">
+                    <p class="panel-title">Accounts</p>
+                    <div class="net-worth-card">
+                        <p class="nw-label">Net worth</p>
+                        <p class="nw-amount" id="netWorth">$0.00</p>
+                    </div>
+                    <div id="accountList"></div>
+                    <button id="addAccountBtn">+ Add account</button>
+                </div>
 
-    loginScreen.style.display = "none"
-    dashboard.style.display = 'block'
-}
+                <div class="panel" id="txPanel">
+                    <p class="panel-title">Transactions</p>
+                    <button id="addTxBtn">+ Add Transaction</button>
+                    <div id="txList"></div>
+                </div>
+            </div>
+         </div>
 
-logoutBtn.onclick = function() {
-    dashboard.style.display = "none"
-    loginScreen.style.display = 'block'
-}
+    </div>
+
+    <script src="app.js"></script>
+</body>
+
+</html>
